294f2ddff4b8c0890bde86c220293b975c7bbfe1
ondal <hiondal@gmail.com>
2024-10-13 15:38:53 +0900

    release

--- update-yaml.sh
+++ update-yaml.sh
@@ -10,11 +10,6 @@ IMAGE_ORG=${IMAGE_ORG:-hiondal}
 read -p "Enter image version (default: 1.0.0): " IMAGE_VERSION
 IMAGE_VERSION=${IMAGE_VERSION:-1.0.0}
 
-# Override default values with provided arguments
-NAMESPACE="$1"
-IMAGE_ORG="$2"
-IMAGE_VERSION="$3"
-
 # Define the deploy directory
 DEPLOY_DIR="deploy"
 
@@ -27,11 +22,11 @@ for file in "${yaml_files[@]}"; do
 done
 
 # Update image full path in each yaml file
-sed -i'' "s@docker.io/hiondal/.*@docker.io/${IMAGE_ORG}/config:${IMAGE_VERSION}@g" ${DEPLOY_DIR}/config.yaml
-sed -i'' "s@docker.io/hiondal/.*@docker.io/${IMAGE_ORG}/eureka:${IMAGE_VERSION}@g" ${DEPLOY_DIR}/eureka.yaml
-sed -i'' "s@docker.io/hiondal/.*@docker.io/${IMAGE_ORG}/subride-front:${IMAGE_VERSION}@g" ${DEPLOY_DIR}/front.yaml
-sed -i'' "s@docker.io/hiondal/.*@docker.io/${IMAGE_ORG}/scg:${IMAGE_VERSION}@g" ${DEPLOY_DIR}/scg.yaml
-sed -i'' "s@docker.io/hiondal/.*@docker.io/${IMAGE_ORG}/subrecommend:${IMAGE_VERSION}@g" ${DEPLOY_DIR}/subrecommend.yaml
+sed -i'' "s@docker.io/[^/]\+/.*@docker.io/${IMAGE_ORG}/config:${IMAGE_VERSION}@g" ${DEPLOY_DIR}/config.yaml
+sed -i'' "s@docker.io/[^/]\+/.*@docker.io/${IMAGE_ORG}/eureka:${IMAGE_VERSION}@g" ${DEPLOY_DIR}/eureka.yaml
+sed -i'' "s@docker.io/[^/]\+/.*@docker.io/${IMAGE_ORG}/subride-front:${IMAGE_VERSION}@g" ${DEPLOY_DIR}/front.yaml
+sed -i'' "s@docker.io/[^/]\+/.*@docker.io/${IMAGE_ORG}/scg:${IMAGE_VERSION}@g" ${DEPLOY_DIR}/scg.yaml
+sed -i'' "s@docker.io/[^/]\+/.*@docker.io/${IMAGE_ORG}/subrecommend:${IMAGE_VERSION}@g" ${DEPLOY_DIR}/subrecommend.yaml
 
 # Notify the user that the changes are complete
 echo "################ Completed ######################"
